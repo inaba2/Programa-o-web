29095d6266117a40b0dd47bcc1a183ce9ca2c4b5
GabrielDCastro <gabrielrangel12@gmail.com>
2019-11-03 14:29:55 -0300

    Tabelas para o banco de dados em relação aos cursos

--- mysite/settings.py
+++ mysite/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'home',
+    'courses',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
